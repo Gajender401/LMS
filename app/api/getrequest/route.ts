648a81ee0f9ac6cf709e4c820bcdf122691b8d64
Gajender401 <kgajender86@gmail.com>
2023-10-08 13:59:42 +0530

    updated 1216

--- app/api/getrequest/route.ts
+++ app/api/getrequest/route.ts
@@ -5,47 +5,50 @@ import { auth } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 
 export async function POST(
-    req: Request
-  ) {
-    try {
-      const user = await currentUser();
+  req: Request
+) {
+  try {
+    const user = await currentUser();
 
-      const {requestId} = await req.json()
-  
-  
-      if (!user || !user.id ) {
-        return new NextResponse("Unauthorized", { status: 401 });
-      }
-  
-  
-      const { userId } = auth();
-  
-      if (!userId || !user.firstName) {
-        return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
-      }
-      
-  
-      const applications = await db.applications.findMany({
-        where: {
-          id: requestId
-        },
-        orderBy: {
-            createdAt: "desc",
-        }
-    });
-  
-     await db.applications.update({
+    const { requestId } = await req.json()
+
+
+    if (!user || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+
+    const { userId } = auth();
+
+    if (!userId || !user.firstName) {
+      return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
+    }
+
+
+    const applications = await db.applications.findMany({
       where: {
         id: requestId
       },
-      data: {
-        status: 'Visited'
+      orderBy: {
+        createdAt: "desc",
       }
-  });
-  
-      return NextResponse.json(applications);
-    } catch (error) {
-      console.log("[Request get]", error);
-      return new NextResponse("Internal Error", { status: 500 })
+    });
+
+    if (applications[0].status == 'Pending') {
+      await db.applications.update({
+        where: {
+          id: requestId
+        },
+        data: {
+          status: 'Visited'
+        }
+      });
     }
+
+
+    return NextResponse.json(applications);
+  } catch (error) {
+    console.log("[Request get]", error);
+    return new NextResponse("Internal Error", { status: 500 })
   }
+}
