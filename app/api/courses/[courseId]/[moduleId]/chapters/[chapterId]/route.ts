5e776aaf730cb2b7d313e8316c69ab947668cdf8
Gajender401 <kgajender86@gmail.com>
2023-10-11 14:27:20 +0530

    updated 1219

--- app/api/courses/[courseId]/[moduleId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/[moduleId]/chapters/[chapterId]/route.ts
@@ -1,17 +1,11 @@
-import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
-const { Video } = new Mux(
-  process.env.MUX_TOKEN_ID!,
-  process.env.MUX_TOKEN_SECRET!,
-);
-
 export async function DELETE(
   req: Request,
-  { params }: { params: { courseId: string; chapterId: string } }
+  { params }: { params: { moduleId: string; chapterId: string } }
 ) {
   try {
     const { userId } = auth();
@@ -20,21 +14,21 @@ export async function DELETE(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const ownCourse = await db.course.findUnique({
-      where: {
-        id: params.courseId,
-        userId,
-      }
-    });
+    // const ownCourse = await db.course.findUnique({
+    //   where: {
+    //     id: params.courseId,
+    //     userId,
+    //   }
+    // });
 
-    if (!ownCourse) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+    // if (!ownCourse) {
+    //   return new NextResponse("Unauthorized", { status: 401 });
+    // }
 
     const chapter = await db.chapter.findUnique({
       where: {
         id: params.chapterId,
-        courseId: params.courseId,
+        moduleId: params.moduleId,
       }
     });
 
@@ -42,22 +36,6 @@ export async function DELETE(
       return new NextResponse("Not Found", { status: 404 });
     }
 
-    if (chapter.videoUrl) {
-      const existingMuxData = await db.muxData.findFirst({
-        where: {
-          chapterId: params.chapterId,
-        }
-      });
-
-      if (existingMuxData) {
-        await Video.Assets.del(existingMuxData.assetId);
-        await db.muxData.delete({
-          where: {
-            id: existingMuxData.id,
-          }
-        });
-      }
-    }
 
     const deletedChapter = await db.chapter.delete({
       where: {
@@ -67,7 +45,7 @@ export async function DELETE(
 
     const publishedChaptersInCourse = await db.chapter.findMany({
       where: {
-        courseId: params.courseId,
+        moduleId: params.moduleId,
         isPublished: true,
       }
     });
@@ -75,7 +53,7 @@ export async function DELETE(
     if (!publishedChaptersInCourse.length) {
       await db.course.update({
         where: {
-          id: params.courseId,
+          id: params.moduleId,
         },
         data: {
           isPublished: false,
@@ -92,7 +70,7 @@ export async function DELETE(
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { courseId: string; chapterId: string } }
+  { params }: { params: { moduleId: string; chapterId: string } }
 ) {
   try {
     const { userId } = auth();
@@ -102,57 +80,16 @@ export async function PATCH(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const ownCourse = await db.course.findUnique({
-      where: {
-        id: params.courseId,
-        userId
-      }
-    });
-
-    if (!ownCourse) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
     const chapter = await db.chapter.update({
       where: {
         id: params.chapterId,
-        courseId: params.courseId,
+        moduleId: params.moduleId,
       },
       data: {
         ...values,
       }
     });
 
-    if (values.videoUrl) {
-      const existingMuxData = await db.muxData.findFirst({
-        where: {
-          chapterId: params.chapterId,
-        }
-      });
-
-      if (existingMuxData) {
-        await Video.Assets.del(existingMuxData.assetId);
-        await db.muxData.delete({
-          where: {
-            id: existingMuxData.id,
-          }
-        });
-      }
-
-      const asset = await Video.Assets.create({
-        input: values.videoUrl,
-        playback_policy: "public",
-        test: false,
-      });
-
-      await db.muxData.create({
-        data: {
-          chapterId: params.chapterId,
-          assetId: asset.id,
-          playbackId: asset.playback_ids?.[0]?.id,
-        }
-      });
-    }
 
     return NextResponse.json(chapter);
   } catch (error) {
